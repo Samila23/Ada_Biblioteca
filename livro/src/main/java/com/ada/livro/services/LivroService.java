2f7440c2ccdeb0deebf8d9fd06c16997531ce8a1
SAMILA MILENA DIAS DE PAULA <60451467+Samila23@users.noreply.github.com>
2023-05-03 22:02:50 -0300

    ultimas atualizações API Livros

--- livro/src/main/java/com/ada/livro/services/LivroService.java
+++ livro/src/main/java/com/ada/livro/services/LivroService.java
@@ -1,64 +1,19 @@
 package com.ada.livro.services;
 
+import com.ada.livro.dto.response.EstoqueResponse;
+import com.ada.livro.model.Livro;
+import com.ada.livro.util.Status;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface LivroService {
 
-import com.ada.livro.dtos.EstoqueDto;
-import com.ada.livro.model.Livro;
-import com.ada.livro.repositories.LivroRepository;
+    Livro save(Livro livro);
+    List<Livro> getAll();
+    Optional<Livro> getById(int id);
+    Livro update(int id, Livro livro);
+    public EstoqueResponse updateEstoque(int id, String tipoTransacao);
+    void delete(int id);
 
-@Service
-public class LivroService {
-	@Autowired
-	LivroRepository livroRepository;
-	
-	public Livro save(Livro livro) {
-		return livroRepository.save(livro);
-	}
-	public List<Livro> getAll(){
-		return livroRepository.findAll();
-	}
-	
-	public Livro getOne(int id) {
-		return livroRepository.findById(id).orElse(new Livro());
-	}
-	
-	public Livro update(int id, Livro livro) {
-		Optional<Livro> optional = livroRepository.findById(id);
-		if (optional.isPresent() == true){ 
-			Livro livroBD = optional.get();
-			 
-			return livroRepository.save(livroBD); 
-		}
-		else {
-			return new Livro();
-		}
-	}
-	
-	public EstoqueDto updateEstoque(int id, int tipoTransacao, Livro livro) { 
-
-		Optional<Livro> optional = livroRepository.findById(id);
-		if (optional.isPresent() == true){
-			Livro livroBD = optional.get();
-			if(tipoTransacao == 1) {
-				//Devolucao
-				livroBD.setQuantidade(livroBD.getQuantidade() + livro.getQuantidade());
-			}
-			else {
-				//Emprestimo
-				livroBD.setQuantidade(livroBD.getQuantidade() - livro.getQuantidade());
-			}
-			return livroRepository.save(livroBD).toDTOEstoque(); 
-		}
-		else {
-			return new Livro().toDTOEstoque();
-		}
-	}
-	
-	public void delete(int id) {
-		livroRepository.deleteById(id);
-	}
 }
---
+++ livro/src/main/java/com/ada/livro/services/LivroServiceImpl.java
@@ -0,0 +1,70 @@
+package com.ada.livro.services;
+
+import com.ada.livro.dto.response.EstoqueResponse;
+import com.ada.livro.exception.NotFoundException;
+import com.ada.livro.model.Livro;
+import com.ada.livro.repositories.LivroRepository;
+import com.ada.livro.util.Status;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.ada.livro.util.Status.DEVOLVIDO;
+import static com.ada.livro.util.Status.EMPRESTADO;
+
+@Service
+@RequiredArgsConstructor
+public class LivroServiceImpl implements LivroService {
+
+    private final LivroRepository livroRepository;
+
+    @Override
+    public Livro save(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    @Override
+    public List<Livro> getAll() {
+        return livroRepository.findAll();
+    }
+
+    @Override
+    public Optional<Livro> getById(int id) {
+        return Optional.ofNullable(livroRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("validation.livro.notfound", id)
+        ));
+    }
+
+    @Override
+    public Livro update(int id, Livro livro) {
+        Optional<Livro> optional = livroRepository.findById(id);
+        if (optional.isPresent()) {
+            Livro livroBD = optional.get();
+            return livroRepository.save(livroBD);
+        }
+        return new Livro();
+    }
+
+    @Override
+    public EstoqueResponse updateEstoque(int id, String tipoTransacao) {
+
+        Optional<Livro> optional = livroRepository.findById(id);
+        if (optional.isPresent()) {
+            Livro livroBD = optional.get();
+            if (tipoTransacao.equalsIgnoreCase(Status.DEVOLVIDO.getStatus())) {
+                livroBD.setQuantidade(livroBD.getQuantidade() + 1);
+            } else if (tipoTransacao.equalsIgnoreCase(Status.EMPRESTADO.getStatus())) {
+                livroBD.setQuantidade(livroBD.getQuantidade() - 1);
+            }
+            return livroRepository.save(livroBD).toDTOEstoque();
+        }
+        return new Livro().toDTOEstoque();
+    }
+
+    @Override
+    public void delete(int id) {
+        livroRepository.deleteById(id);
+    }
+}
